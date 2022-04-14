463039f543c203c232a809050729d085c5d18834
tosif91 <61594032+tosif91@users.noreply.github.com>
2022-04-14 12:33:41 +0530

    build instagram model

---
+++ oops/instagram/Comment.py
@@ -0,0 +1,8 @@
+class Comment:
+    def __init__(self,cid,comment,date ,uid ,likes,pid):
+        self.cid = cid 
+        self.comment = comment 
+        self.date = date 
+        self.uid = uid 
+        self.likes = likes 
+        self.pid = pid
---
+++ oops/instagram/Post.py
@@ -0,0 +1,9 @@
+class Post:
+    def __init__(self,pid,image,date ,like,share,comments,uid):
+        self.pid = pid 
+        self.image = image 
+        self.date = date 
+        self.like = like 
+        self.share = share 
+        self.comments = comments 
+        self.uid = uid 
